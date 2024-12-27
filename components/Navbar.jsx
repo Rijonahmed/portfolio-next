5dc8fe02ff989d7152aef979b900970bce682639
Rijonahmed <rijon036@gmail.com>
2024-12-27 23:43:00 +0600

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>hello</div>
+            
+        </nav>
+    );
+};
+
+export default Navbar;
